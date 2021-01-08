3e8dad2488e6549deb902cc5d1c9670bb1e595d6
sjoerd dronkers <sjoerddronkers@MacBook-Pro-van-sjoerd-3.local>
2021-01-08 15:29:36 +0100

    fixed invalid syntax error after if statement

--- code/classes/traject.py
+++ code/classes/traject.py
@@ -1,22 +1,32 @@
 class Traject():
+    """
+    """
     def __init__(self, uid):
         self.traject_id = uid
         self.stations = []
 
     def add_station(self, node):
+        """
+        """
         self.stations.append(node)
 
     def get_stations(self):
+        """
+        """
         return self.stations
 
     def get_traject_time(self):
+        """
+        """
         station_1 = None
         traject_time = 0
         time = 0
+
         for station in self.stations:
-            if station_1 is not None
+            if station_1 is not None:
                 connections = station_1.get_connections
                 time = connections[station]
             traject_time = traject_time + time
             station_1 = station
+
         return traject_time
