6e5b03b335218b3e1201fa3ceec3bb1848c46e46
jojoJochem <jochemgvg@gmail.com>
2021-01-14 18:18:14 +0100

    update depth.py

--- code/algorithms/depth.py
+++ code/algorithms/depth.py
@@ -1,4 +1,4 @@
-from code.classes import traject
+from code.classes import route
 import copy
 
 class depth():
@@ -7,163 +7,66 @@ class depth():
         self.max_trajects = max_trajects
         self.time_frame = time_frame
 
-        states = list(self.graph.nodes.values())
-        states.sort(key=lambda node: (len(node.connections.values()), min(node.connections.values())))
+        self.states = [copy.deepcopy(self.graph)]
 
-        self.stations = [copy.deepcopy(states)]
+        self.stations = list(self.graph.nodes.values())
+        self.best_solution = None
+        self.best_value = 0
 
-    def get_next_state(self, current_station):
+    def get_next_state(self):
+        """
+        Method that gets the next state from the list of states.
+        """
+        return self.states.pop()
 
+    def build_children(self, graph, node, route_id):
+        """
+
+        """
+        values = node.get_connections()
+
+        for value in values:
+            new_graph = copy.deepcopy(graph)
+            new_graph.routes[route_id].add_station(value)
+            self.states.append(new_graph)
+
+
+    def get_next_node(self, graph, route_id):
+        return graph.routes[route_id][-1]
+
+    def check_solution(self, new_graph):
+        result = new_graph.get_result()
+            if result > self.best_value:
+                self.best_solution = new_graph
+                self.best_value = result
+                print(f"New best value: {self.best_value}")
+
+    def starting_station (self, graph, route_id):
+        for station in self.stations:
+            new_graph = copy.deepcopy(graph)
+            router = route.Route(route_id)
+            new_graph.add_route(router)
+            new_graph.routes[route_id].add_station(station)
+            self.states.append(new_graph)
 
     def run(self):
+        max_time = self.max_trajects * self.time_frame
+        current_route = 0
         while self.states:
-            start = self.stations.pop()
-            train = traject.Traject(1)
-            self.graph.add_traject(train)
-            dfs(start, train)
+            new_graph = self.get_next_state()
 
-
-    def dfs(self,child, traject)
-        values = child.get_connections()
-        for value in values:
-            train.add_station(value)
-            if train.get_traject_time() < self.time_frame:
-                if self.graph.get_connections_p_value == 1:
-                    solutions += 1
-                    if self.graph.get_result() > optimal_result:
-                        optimal_result = self.graph.get_result()
-                    train.remove_station
-            dfs(value)
-
-
-
-
-
-
-
-
-
-
-
-
-    """def run(self):
-        traject_id = 1
-        traject_path = traject.Traject(1)
-        stack = [""]
-
-        # iterate until every possible starting location is finished
-        while self.states:
-            #start from new location after every finished graph
-            new_graph = self.states.pop()
-
-            # set first node as start of traject
-            node = new_graph
-            traject_path.add_station(node)
-
-            # get possible connecting stations from node
-            values = node.get_connections()
-
-            for value in values:
-                child = copy.deepcopy(state)
-                child += value
-
-
-
-
-    def dfs(self, child)
-            traject.add_station(current_node)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            if stack.get_traject_time < (self.time_frame * self.max_trajects):
-                for value in state.get_connections():
-                    child = copy.deepcopy(state)
-                    child
-                    stack.add_station(child)
-
-            for state in self.states:
-                node = state
-
-                for i in state.get_connections()
-                    child = copy.deepcopy(new_traject)
-                    child += i
-                    stack.add_station(child)
-            else if traject_id == self.max_trajects:
-                return optimal_state
+            if current_route <= self.max_trajects and not new_graph.routes[current_route]:
+                self.starting_station(new_graph, current_route)
+            elif current_route <= self.max_trajects
+                if new_graph.routes[current_route].get_route_time() < self.time_frame:
+                    if new_graph.get_connections_p_value == 1:
+                        self.check_solution(new_graph)
+                    else:
+                        self.check_solution(new_graph)
+                        self.build_children(new_graph, node, current_route)
+                else:
+                    new_graph.routes[route_id].remove_station(node)
+                    self.check_solution(new_graph)
+                    current_route += 1
             else:
-                traject_id += 1
-
-
-        while traject_id != 0:
-            new_graph = self.states.pop()
-
-            values = new_graph.get_connections()
-
-            for value in values
-                child = copy.deepcopy(new_graph)
-                child += i
-                stack.add_station(child)
-
-
-
-        depth = 200
-        traject_id = 1
-        stack = traject.Traject(traject_id)
-        stack.append('')
-
-        while len(stack) > 0:
-            state = stack.pop()
-            print(state)
-            if stack.get_traject_time < self.time_frame:
-                for i in state.get_connections()
-                    child = copy.deepcopy(state)
-                    child += i
-                    stack.add_station(child)
-            else if traject_id == self.max_trajects:
-                return optimal_state
-            else:
-                traject_id += 1
-
-    def dfs_non_recursive(self):
-        graph = list(self.graph.nodes.values())
-        graph.sort(key=lambda node: (len(node.connections.values()), min(node.connections.values())))
-
-        traject_id = 1
-
-        for source in graph:
-            stack = [source]
-            # while traject_id <= self.max_trajects:
-            path = traject.Traject(traject_id)
-
-            while path.get_traject_time() < self.time_frame:
-                state = stack.pop()
-
-                if state not in path:
-                    path.append(s)
-
-                if state not in graph:
-                    #leaf node
-                    continue
-
-                for neighbor in graph[s]:
-                    stack.append(neighbor)
-
-                return " ".join(path)"""
+                current_route -= 1
