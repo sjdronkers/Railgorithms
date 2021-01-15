734c920d48e712bb4ed1e56bfafc99d188ed3f04
jojoJochem <jochemgvg@gmail.com>
2021-01-15 17:54:33 +0100

    depth.py working version

--- code/algorithms/depth.py
+++ code/algorithms/depth.py
@@ -1,4 +1,3 @@
-from code.classes import route
 import copy
 
 class Depth():
@@ -9,7 +8,6 @@ class Depth():
 
         self.states = [copy.deepcopy(self.graph)]
 
-        self.stations = list(self.graph.nodes.values())
         self.best_solution = None
         self.best_value = 0
 
@@ -19,24 +17,18 @@ class Depth():
         """
         return self.states.pop()
 
-    def build_children(self, graph, node, route_id):
+    def build_children(self, graph, city, route_id):
         """
 
         """
-        values = node.get_connections()
+        connections = graph.nodes[city].get_connections()
 
-        for value in values:
+        for connection in connections:
             new_graph = copy.deepcopy(graph)
-            new_graph_value = self.get_new_graph_node(new_graph, value)
-            new_graph.routes[route_id].add_station(new_graph_value)
+            new_graph.add_station(connection, route_id)
             self.states.append(new_graph)
 
-    def get_new_graph_node(self, graph, node):
-        for station in graph.nodes:
-            if station == node.city:
-                return graph.nodes[station]
-
-    def get_next_node(self, graph, route_id):
+    def get_next_station(self, graph, route_id):
         return graph.routes[route_id].stations[-1]
 
     def check_solution(self, graph):
@@ -47,36 +39,43 @@ class Depth():
             print(f"New best value: {self.best_value}")
 
     def starting_station (self, graph, route_id):
-        for station in self.stations:
+        for station in list(self.graph.nodes):
             new_graph = copy.deepcopy(graph)
-            router = route.Route(route_id)
-            new_graph.add_route(router)
-            new_graph.routes[route_id].add_station(station)
+            new_graph.add_route(route_id)
+            new_graph.add_station(station, route_id)
             self.states.append(new_graph)
 
     def run(self):
-        current_route = 0
+        current_route = 1
+        last_station = None
+
         while self.states:
             new_graph = self.get_next_state()
 
-            try:
-                new_graph.routes[current_route]
-            except:
-                if current_route <= self.max_trajects:
-                    self.starting_station(new_graph, current_route)
-            else:
-                if current_route <= self.max_trajects:
-                    if new_graph.routes[current_route].get_route_time() < self.time_frame:
-                        if new_graph.get_connections_p_value == 1:
-                            self.check_solution(new_graph)
-                        else:
-                            self.check_solution(new_graph)
-                            node = self.get_next_node(new_graph, current_route)
-                            self.build_children(new_graph, node, current_route)
-                    else:
-                        new_graph.routes[route_id].remove_station(node)
+            if new_graph.routes.get(current_route) == None and current_route <= self.max_trajects:
+                self.starting_station(new_graph, current_route)
+            elif current_route <= self.max_trajects:
+                if new_graph.get_route_time(current_route) < self.time_frame:
+                    if new_graph.get_connections_p_value == 1:
                         self.check_solution(new_graph)
-                        current_route += 1
+                    else:
+                        self.check_solution(new_graph)
+                        last_station = self.get_next_station(new_graph, current_route)
+                        self.build_children(new_graph, last_station, current_route)
                 else:
-                    current_route -= 1
+                    new_graph.remove_station(last_station, current_route)
+                    self.check_solution(new_graph)
+                    current_route += 1
+            else:
+                current_route -= 1
+
+
+
+"""    def get_new_graph_node(self, graph, node):
+        for station in graph.nodes:
+            if station == node.city:
+                return graph.nodes[station]
+
+new_graph_value = self.get_new_graph_node(new_graph, value)
+"""
 
