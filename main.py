104e793784971ec5bffd29d45b6c34059b0da1f3
sjoerd dronkers <sjoerddronkers@MacBook-Pro-van-sjoerd-3.local>
2021-01-18 15:28:09 +0100

    Added algorithm dictionary

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
-from code.algorithms import greedy as gr
 from code.classes import graph
+
+from code.algorithms import greedy as gr
+from code.algorithms import randomise as ra
+from code.algorithms import depth as de
+from code.algorithms import hillclimber as hi
+
 from code.visualisation import visualise as vis
 
 import csv
 
 if __name__ == "__main__":
 
+    #----------------------- User input ------------------------------------------
     # get user input for the region, default to Nationaal
     region = input("Do you want routes for (H)olland, (N)ational or (A)lkmaar? ")
-    region = region.lower()
-    if region == "holland" or region == "h":
+    if region[0].lower() == "h":
         region = "Holland"
-    elif region == "alkmaar" or region == "a":
+    elif region[0].lower() == "a":
         region = "Alkmaar"
     else:
         region = "Nationaal"
+
+    print(f"The graph used is {region}")
 
     # create graph
     usable_graph = graph.Graph(f"data/{region}/Stations{region}.csv",
@@ -22,38 +29,89 @@ if __name__ == "__main__":
     
     # ask if the user wants a visualization
     want_anim = input("Do you want an animation? (Y)es or (N)o? ")
-    want_anim = want_anim.lower()
-    if want_anim in "no":
+    if want_anim.lower() in "no":
         want_anim = False
 
-    # getting the amount of trajectories and time frame
+    # getting the amount of routes and check if valid
     train_amount = int(input("What is the maximum amount of routes? "))
-    time_frame = int(input("What is the time frame in minutes? "))
-
-    # check for valid user input
     if not isinstance(train_amount, int) or train_amount < 0:
         train_amount = 7
+        print(f"Invalid amount of routes, defaulted to {train_amount}")
+
+    # get time frame and check if valid
+    time_frame = int(input("What is the time frame in minutes? "))
     if not isinstance(time_frame, int) or time_frame < 0:
         time_frame = 120
+        print(f"Invalid time frame, default time set to {time_frame}")
 
-    # Dit zijn de waarden van opdracht 1 op de RailNL pagina.
-    greedy = gr.Greedy(usable_graph, train_amount, time_frame)
-    greedy.run()
 
-    vis.visualise(greedy.graph, want_anim)
+    #----------------------- Algorithm choice ----------------------------------------
+    algo_dict = {
+        "Greedy": gr.Greedy(usable_graph, train_amount, time_frame),
+        "Random": ra.Randomise(usable_graph, train_amount, time_frame),
+        "Depth first": de.Depth(usable_graph, train_amount, time_frame)
+    }
 
-    # create csv file for output
+    # get users' algorithm choice
+    algo_choice = input("Would you like a (R)andom, (G)reedy, (D)epth first or (I)terative algorithm? ")
+    algo_choice = algo_choice.upper()
+
+    # check if the user wants an iterative algorithm
+    if algo_choice[0] == "I":
+        print("Which base state dow you want:")
+        base_state = input("(R)andom, (G)reedy or (D)epth first? ")
+
+        # check for valid input, default to Random
+        if base_state[0] not in "RGD":
+            base_algo = algo_dict['Random']
+        else:
+            base_algo = [algo for word, algo in algo_dict.items() if word.startswith(base_state)]
+            base_algo = base_algo[0]
+            print(base_algo)
+
+        base_algo.run()
+
+        # notify user base state has been created
+        print("Base state is created successfully!")
+        iterate = int(input("How many iterations do you want? "))
+
+        # create hillclimber object
+        used_algo = hi.HillClimber(base_algo.graph, train_amount, time_frame)
+
+    # user wants random, greedy or depth first algorithm
+    elif algo_choice[0] in "RGD":
+        used_algo = [algo for word, algo in algo_dict.items() if word.startswith(algo_choice)]
+        used_algo = used_algo[0]
+
+    # invalid user input, default to random
+    else:
+        print("Invalid input, ran Random algorithm instead.")
+        used_algo = algo_dict['Random']
+
+    # check if hillclimber is used
+    if algo_choice[0] == "I":
+        used_algo.run(iterate)
+    else:
+        used_algo.run()
+
+
+    #------------------------ Visualisation ------------------------------------------
+    vis.visualise(used_algo.graph, want_anim)
+
+
+    #------------------------ Create CSV ---------------------------------------------
     with open('results/output.csv', 'w', newline='') as file:
         writer = csv.writer(file)
 
         # write the header
         writer.writerow(["train", "stations"])
-
-        # write every row with the corresponding train number
-        for train_number, traj in enumerate(greedy.graph.trajects, start=1):
-            stations = ' '.join([f"{station.city}," for station in traj.get_stations()])
-            stations = stations[:-1]
+      
+        # write the routes onto the csv file
+        train_number = 1
+        for route in used_algo.graph.routes:
+            stations = ", ".join(used_algo.graph.routes[route].get_stations())
             writer.writerow(["train_" + str(train_number), f"[{stations}]"])
+            train_number += 1
 
         # write the footer
-        writer.writerow(["score", greedy.graph.get_result()])
+        writer.writerow(["score", used_algo.graph.get_result()])
