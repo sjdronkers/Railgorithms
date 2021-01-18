6f45ab5a1e5b4a911477c0f695a7dd5cd502915b
jojoJochem <jochemgvg@gmail.com>
2021-01-18 14:16:40 +0100

    update depth.py with pruning

--- code/algorithms/depth.py
+++ code/algorithms/depth.py
@@ -1,15 +1,19 @@
 import copy
+import random
+import math
 
 class Depth():
     def __init__(self, graph, max_trajects, time_frame):
         self.graph = copy.deepcopy(graph)
         self.max_trajects = max_trajects
         self.time_frame = time_frame
+        self.stations = random.sample(list(self.graph.nodes), len(list(self.graph.nodes)))
 
         self.states = [copy.deepcopy(self.graph)]
 
         self.best_solution = None
         self.best_value = 0
+        self.counter = 0
 
     def get_next_state(self):
         """
@@ -39,25 +43,37 @@ class Depth():
             print(f"New best value: {self.best_value}")
 
     def starting_station (self, graph, route_id):
-        for station in list(self.graph.nodes):
+        for station in self.stations:
             new_graph = copy.deepcopy(graph)
+            if new_graph.routes.get(route_id) != None:
+                del new_graph.routes[route_id]
             new_graph.add_route(route_id)
             new_graph.add_station(station, route_id)
             self.states.append(new_graph)
+
+    def pruning(self, prune_type, graph, route_id):
+        if prune_type == 1:
+            return (graph.get_result() < (10000 * ((route_id - 1) / self.max_trajects) - (100 * (route_id - 1) - (self.time_frame) * (route_id - 1))))
+        else:
+            return False
 
     def run(self):
         current_route = 1
         last_station = None
 
+
         while self.states:
             new_graph = self.get_next_state()
+            self.counter += 1
 
             if new_graph.routes.get(current_route) == None and current_route <= self.max_trajects:
                 self.starting_station(new_graph, current_route)
             elif current_route <= self.max_trajects:
                 if new_graph.get_route_time(current_route) < self.time_frame:
-                    if new_graph.get_connections_p_value == 1:
+                    if new_graph.get_connections_p_value() == 1:
                         self.check_solution(new_graph)
+                    elif current_route > 1 and (new_graph.get_result() < (10000 * ((current_route - 1) / self.max_trajects) - (100 * (current_route - 1) - (self.time_frame) * (current_route - 1)))):
+                        current_route -= 1
                     else:
                         self.check_solution(new_graph)
                         last_station = self.get_next_station(new_graph, current_route)
@@ -68,14 +84,8 @@ class Depth():
                     current_route += 1
             else:
                 current_route -= 1
+            """for route in new_graph.routes.values():
+                print(route.route_id, route.stations)"""
 
-
-
-"""    def get_new_graph_node(self, graph, node):
-        for station in graph.nodes:
-            if station == node.city:
-                return graph.nodes[station]
-
-new_graph_value = self.get_new_graph_node(new_graph, value)
-"""
+        self.graph = self.best_solution
 
